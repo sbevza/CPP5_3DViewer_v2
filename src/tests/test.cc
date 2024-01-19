67120afb47a7d691c5baf0e460e24f0285f9982a
Your Name <you@example.com>
2024-01-19 21:23:39 +0300

    test fix

--- src/tests/test.cc
+++ src/tests/test.cc
@@ -52,42 +52,42 @@ TEST(ParserTest, ParseObjTest4) {
   ASSERT_FALSE(parser.hasError());
 }
 
-// TEST(ParserTest, ParseInvalidObjTest1) {
-//   s21::Attrib attrib;
-//   std::string filename = "./objs/zero_size.obj";
+TEST(ParserTest, ParseInvalidObjTest1) {
+  s21::Attrib attrib;
+  std::string filename = "./objs/zero_size.obj";
 
-//   s21::Parser parser;
-//   parser.parseObj(attrib, filename);
+  s21::Parser parser;
+  parser.parseObj(attrib, filename);
 
-//   ASSERT_FALSE(!parser.hasError());
-// }
+  ASSERT_FALSE(!parser.hasError());
+}
 
-// TEST(ParserTest, ParseInvalidObjTest2) {
-//   s21::Attrib attrib;
-//   std::string filename = "./objs/invalid_objs_file.obj";
+TEST(ParserTest, ParseInvalidObjTest2) {
+  s21::Attrib attrib;
+  std::string filename = "./objs/invalid_objs_file.obj";
 
-//   s21::Parser parser;
-//   parser.parseObj(attrib, filename);
+  s21::Parser parser;
+  parser.parseObj(attrib, filename);
 
-//   ASSERT_FALSE(!parser.hasError());
-// }
+  ASSERT_FALSE(!parser.hasError());
+}
 
-// TEST(ParserTest, ParseInvalidObjTest3) {
-//   s21::Attrib attrib;
-//   std::string filename = "";
+TEST(ParserTest, ParseInvalidObjTest3) {
+  s21::Attrib attrib;
+  std::string filename = "";
 
-//   s21::Parser parser;
-//   parser.parseObj(attrib, filename);
+  s21::Parser parser;
+  parser.parseObj(attrib, filename);
 
-//   ASSERT_FALSE(!parser.hasError());
-// }
+  ASSERT_FALSE(!parser.hasError());
+}
 
-// TEST(ParserTest, ParseInvalidObjTest4) {
-//   s21::Attrib attrib;
-//   std::string filename = "objs";
+TEST(ParserTest, ParseInvalidObjTest4) {
+  s21::Attrib attrib;
+  std::string filename = "objss";
 
-//   s21::Parser parser;
-//   parser.parseObj(attrib, filename);
+  s21::Parser parser;
+  parser.parseObj(attrib, filename);
 
-//   ASSERT_FALSE(!parser.hasError());
-// }
+  ASSERT_FALSE(!parser.hasError());
+}
